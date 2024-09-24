1371ec086c51876eaa411d9d868c365bac6107fa
KoDleGaCY7500 <notkodlegacy7500@gmail.com>
2024-09-24 08:20:47 -0700

    homeask (intro to tkinter)

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #Intrototkinter
+import tkinter
+window = tkinter.Tk()
+window.title("Button GUI")
+button_widget = tkinter.Button(window,text="Welcome to DataCamp's Tutorial on Tkinter")
+button_widget.pack()
+tkinter.mainloop()
